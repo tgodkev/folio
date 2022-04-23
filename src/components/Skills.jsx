8a367a4264ab924c23bd2e1cb4eb5af07f9c2f27
kevin knight <tgodkev@gmail.com>
2022-04-23 00:27:03 -0400

    style changes. icons added

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -7,43 +7,53 @@ import node from '../images/nodeimg.png'
 import firebase from '../images/firebaseimg.png'
 import react2 from '../images/react2.png'
 import npmimg from '../images/npmimg.png'
+import shopify from '../images/shopify.png'
+import wordpress from '../images/wordpress.png'
 function Skills() {
   return (
+    
     <div className='skills'>
-        <h1>Technologies</h1>
+    <h1 className='skillstitle'>Technologies</h1>
         <div className='container'>
         <div class='skillz'>
-        <span class="tooltip" data-tooltip="HTML5"><img src={html} alt="" /></span>
+        <span class="tooltip" data-tooltip="HTML5"><img src={html} alt="html icon" /></span>
         </div>
 
         <div class='skillz'>
-        <span class="tooltip" data-tooltip="CSS3"><img src={css} alt="" /></span>
+        <span class="tooltip" data-tooltip="CSS3"><img src={css} alt="css icon" /></span>
         </div>    
 
         <div class='skillz'>
-        <span class="tooltip" data-tooltip="Javascript"><img src={javascript} alt="" /></span>
+        <span class="tooltip" data-tooltip="Javascript"><img src={javascript} alt="javascript icon" /></span>
         </div>
         
         <div class='skillz'>
-        <span class="tooltip" data-tooltip="React.js"><img src={react2} alt="" /></span>
+        <span class="tooltip" data-tooltip="React.js"><img src={react2} alt="react icon" /></span>
         </div>
 
         <div class='skillz'>
-        <span class="tooltip" data-tooltip="Node.js"><img src={node} alt="" /></span>
+        <span class="tooltip" data-tooltip="Node.js"><img src={node} alt="node.js icon" /></span>
         </div>
       
         <div class='skillz'>
-        <span class="tooltip" data-tooltip="MongoDb"><img src={mongo} alt="" /></span>
+        <span class="tooltip" data-tooltip="MongoDb"><img src={mongo} alt="mongodb icon " /></span>
         </div>
       
         <div class='skillz'>
-        <span class="tooltip" data-tooltip="Firebase"> <img src={firebase} alt="" /></span>
+        <span class="tooltip" data-tooltip="Firebase"> <img src={firebase} alt="fire base icon" /></span>
         </div>
         
         <div class='skillz'>
-        <span class="tooltip" data-tooltip="NPM."><img src={npmimg} alt="" /></span>
+        <span class="tooltip" data-tooltip="NPM."><img src={npmimg} alt="npm icon" /></span>
         </div>
         
+        <div class='skillz'>
+        <span class="tooltip" data-tooltip="Shopify."><img src={shopify} alt="shopify icon" /></span>
+        </div>
+        
+        <div class='skillz'>
+        <span class="tooltip" data-tooltip="Wordpress."><img src={wordpress} alt="wordpress icon" /></span>
+        </div>
        
         </div>
     </div>
